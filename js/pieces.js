74adb866567d43c79b57f5806c9c0bdd73f738c9
App Academy Student <anujan.panchadcharam@uoit.net>
2013-10-30 15:15:13 -0400

    Piece Classes minus some

--- js/pieces.js
+++ js/pieces.js
@@ -1,12 +1,10 @@
-
-var Piece = function() {};
-// inhertiable attributes / methods
-Piece.prototype = {
-  board       : null,
-  color       : '',
-  pos         : [],
-  move_coords : [],
-  valid_moves : function(){
+var Piece = Class.extend({
+  init: function(options) {
+    this.board = options.board;
+    this.color = options.color;
+    this.pos = options.pos;
+  },
+  valid_moves: function(){
     var moves = this.moves();
     var valid_moves = [];
     for (var move in moves) {
@@ -20,110 +18,117 @@ Piece.prototype = {
     new_board.move(this.pos, pos, true);
     return new_board.checked(this.color);
   }
-};
+});
 
-class InvalidMoveException < StandardError
-end
+var SlidingPiece = Piece.extend({
+  move_dirs: function(cord) {
+    var moves = [];
+    var i = 1;
+    while (true) {
+      var pos = [this.pos[0] + (cord[0] * i), this.pos[1] + (cord[1] * i)];
+      if (this.board.off_the_grid(pos)) {
+        break;
+      }
+      var hit_piece = this.board.pieces[pos[0]][pos[1]];
+      if (hit_piece) {
+        if (hit_piece.color != this.color) {
+          moves.push(pos);
+          break;
+        }
+      }
+      moves.push(pos);
+      i++;
+    }
+    return moves;
+  },
+  moves: function() {
+    var moves = [];
+    var coords = this.move_coords;
+    for (var move in coords) {
+      moves.push(this.move_dirs(move));
+    }
+    return moves;
+  }
+});
 
-class SlidingPiece < Piece
-  def move_dirs(cord)
-
-    moves = []
-    i =  1
-    while true
-      pos = [self.pos[0] + (cord[0]* i), self.pos[1] + (cord[1] * i)]
-      break if self.board.off_the_grid?(pos)
-      hit_piece = self.board.pieces[pos[0]][pos[1]]
-      if !hit_piece.nil?
-        moves << pos if hit_piece.color != self.color
-        break
-      end
-      moves << pos
-      i += 1
-    end
-
-    moves
-  end
-
-  def moves
-    self.move_coords.inject([]) do |moves,dir|
-      moves + move_dirs(dir)
-    end
-  end
-end
-
-var SteppingPiece = function() {};
-SteppingPiece.prototype = {
+var SteppingPiece = Piece.extend({
   moves: function() {
     var move_arr = [];
     for (var move in this.move_coords) {
-      x = move[0] + this.pos[0];
-      y = move[1] + this.pos[1];
+      var x = move[0] + this.pos[0];
+      var y = move[1] + this.pos[1];
       move_arr.push([x, y]);
     }
-    ret_arr = []
-    for (var move in move_arr) {
-      if (!this.board.off_the_grid(move)) {
-        space = this.board.pieces[move[0]][move[1]];
-        if (space == null || space.color != this.color)
-        ret_arr.push(space.pos)
+    var ret_arr = [];
+    for (var mv in move_arr) {
+      if (!this.board.off_the_grid(mv)) {
+        var space = this.board.pieces[mv[0]][mv[1]];
+        if (!space || space.color != this.color) {
+        	ret_arr.push(space.pos);
+        }
       }
     }
     return move_arr;
   }
-}
+});
 
-class Pawn < SteppingPiece
-  attr_accessor :passant
-  def initialize(options)
-    super(options)
-    @passant = false
-  end
-  def to_s
-    self.color == :white ? "\u2659" : "\u265F"
-  end
-  def moves
-    moves = []
+var Pawn = SteppingPiece.extend({
+  init: function(options) {
+    var that = this._super(options);
+    that.passant = false;
+    return that;
+  },
+  to_s: function() {
+    return this.color == "white" ? "\u2659" : "\u265F";
+  },
+  moves: function() {
+    var moves = [];
+    var attack_arr = [[1,1], [1,-1]];
+    for (var attack in attack_arr) {
+      if (this.color == "white") {
+        attack[0] = -attack[0];
+      }
+      var space = this.board.pieces[this.pos[0] + attack[0]][this.pos[1] + attack[1]];
+      if (space && space.color != this.color) {
+        moves.push([this.pos[0] + attack[0], this.pos[1] + attack[1]]);
+      }
+    }
+    var last = this.board.last_moved;
+    if (last instanceof Pawn && last.passant && Math.abs((last.pos[1]-this.pos[1])) == 1) {
+      if (this.color == "black" && this.pos[0] == 4) {
+        moves.push([last.pos[0]+1,last.pos[1]]);
+      } else if (this.color == "white" && this.pos[0] == 3) {
+        moves.push([last.pos[0]-1,last.pos[1]]);
+      }
+    }
+    var steps = [[1, 0]];
+    if ((this.color == "white" && this.pos[0] == 6) || (this.color == "black" && this.pos[1] == 1)) {
+      steps.push([2,0]);
+    }
+    for (var step in steps) {
+      if (this.color == "white") {
+        step[0] = -(step[0]);
+      }
+      var space = this.board.pieces[this.pos[0] + step[0]][this.pos[1] + step[1]];
+      if (!space) {
+        moves.push[this.pos[0] + step[0],this.pos[1] + step[1]];
+      }
+    }
+    return moves;
+   }
+});
 
-    [[1,1],[1,-1]].each do |attack|
-      attack[0] = -attack[0] if self.color == :white
-      space = self.board.pieces[pos[0] + attack[0]][pos[1] + attack[1]]
-      moves << [pos[0] + attack[0], pos[1] + attack[1]] unless space.nil? || space.color == self.color
-    end
-    last = self.board.last_moved
-    if last.is_a?(Pawn) && last.passant && (last.pos[1]-self.pos[1]).abs == 1
-      moves << [last.pos[0]+1,last.pos[1]] if self.color == :black && self.pos[0] == 4
-      moves << [last.pos[0]-1,last.pos[1]] if self.color == :white && self.pos[0] == 3
-    end
+var Knight = SteppingPiece.extend({
+    init: function(options) {
+      var that = this._super(options);
+      that.move_coords = [[1,2],[-1,2],[1,-2],
+    [-1,-2],[2,1],[-2,1],[-2,-1],[2,-1]];
+    },
 
-    steps = [[1,0]]
-    steps << [2,0] if self.color == :white && self.pos[0] == 6
-    steps << [2,0] if self.color == :black && self.pos[0] == 1
-
-    steps.each do |move|
-      move[0] = -move[0] if self.color == :white
-      space = self.board.pieces[pos[0] + move[0]][pos[1] + move[1]]
-      moves << [pos[0] + move[0],pos[1] + move[1]] if space.nil?
-    end
-
-    moves
-  end
-end
-
-class Knight < SteppingPiece
-  KNIGHT_MOVES = [[1,2],[-1,2],[1,-2],
-    [-1,-2],[2,1],[-2,1],[-2,-1],[2,-1]]
-
-  def initialize(options)
-    super
-    @move_coords = KNIGHT_MOVES
-  end
-
-  def to_s
-    self.color == :white ? "\u2658" : "\u265E"
-  end
-end
-
+    to_s: function() {
+      return this.color == "white" ? "\u2658" : "\u265E";
+    }
+});
 class King < SteppingPiece
   attr_accessor :moved
   KING_MOVES = [[1, 0], [0, 1], [-1, 0], [0, -1],
@@ -181,3 +186,10 @@ class Queen < SlidingPiece
     self.color == :white ? "\u2655" : "\u265B"
   end
 end
+
+var Queen = function() {};
+Queen.prototype = {
+  var QUEEN_MOVES = [[1, 0], [0, 1], [-1, 0], [0, -1],
+    [1, 1], [-1, 1], [-1, -1], [1, -1]];
+
+};
